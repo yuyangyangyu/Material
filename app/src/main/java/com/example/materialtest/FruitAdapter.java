846a997f5a3de76aed3ebb1d4f7b37978dcfe488
yuyangyangyu <1813233580@qq.com>
2019-04-04 20:38:26 +0800

    新增对城市的美食获取

--- app/src/main/java/com/example/materialtest/FruitAdapter.java
+++ app/src/main/java/com/example/materialtest/FruitAdapter.java
@@ -12,9 +12,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
-
 import java.util.List;
 
 
